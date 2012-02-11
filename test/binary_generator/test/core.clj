8afd4fe25001710fdd6237894f2e1ac471a9c52d
Dương Yang ヤン Nguyễn <cmpitg@gmail.com>
2012-02-11 15:05:43 +0700

    SergeStinckwich

--- test/binary_generator/test/core.clj
+++ test/binary_generator/test/core.clj
@@ -5,7 +5,7 @@
 (defn binary-generator [length]
   (cond (= 0 length) '()
         (= 1 length) '((0) (1))
-        (= 2 length) (concat (list (conj '(0) 0)) (list (conj '(0) 1)) (list (conj '(1) 0)) (list (conj '(1) 1))))
+        (= 2 length) (concat (list (conj '(0) 0)) (list (conj '(1) 0)) (list (conj '(0) 1)) (list (conj '(1) 1))))
 
   )
 

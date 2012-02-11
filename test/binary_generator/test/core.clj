222c76d57b3d64ed3291ea4a4203fcd638cbf2c8
Dương Yang ヤン Nguyễn <cmpitg@gmail.com>
2012-02-11 15:21:19 +0700

    tbble

--- test/binary_generator/test/core.clj
+++ test/binary_generator/test/core.clj
@@ -5,18 +5,12 @@
 (defn binary-generator [length]
   (cond (= 0 length) '()
         (= 1 length) '((0) (1))
-        (= 2 length) (concat (list (apply conj
-                                          (nth (binary-generator 1) 0)
-                                          (list 0)))
-                             (list (apply conj
-                                          (nth (binary-generator 1) 1)
-                                          (list 0)))
-                             (list (apply conj
-                                          (nth (binary-generator 1) 0)
-                                          (list 1)))
-                             (list (apply conj
-                                          (nth (binary-generator 1) 1)
-                                          (list 1)))))
+        (= 2 length) (concat (list (map conj
+                                          (repeat (nth (binary-generator 1) 0))
+                                          '(0 1)))
+                             (list (map conj
+                                          (repeat (nth (binary-generator 1) 1))
+                                          '(0 1)))))
   )
 
 (deftest length-0-return-empty-sequence

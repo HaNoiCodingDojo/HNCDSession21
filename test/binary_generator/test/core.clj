1440d5c31e95f05f5fd8c2ccfd69fa9620be47ed
Dương Yang ヤン Nguyễn <cmpitg@gmail.com>
2012-02-11 14:39:50 +0700

    cmpitg

--- test/binary_generator/test/core.clj
+++ test/binary_generator/test/core.clj
@@ -2,5 +2,8 @@
   (:use [binary-generator.core])
   (:use [clojure.test]))
 
+(defn binary-generator (length)
+  '())
+
 (deftest length-0-return-empty-sequence
   (is (= '() (binary-generator 0))))
